4bdcbc34918212cb735da5a89ae4621983606436
Pavel <pav.timashkov@gmail.com>
2021-08-26 21:54:29 +0300

    firts commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict "
+
+/*const obj={
+    name:"Павел",
+    age:27,
+    isMarried:false
+};
+console.log(obj.name);
+*/
+//const name=prompt("Как тебя зовут?");
+const name=prompt("Как вас зовут");
+document.write(`Привет , ${name}`);
