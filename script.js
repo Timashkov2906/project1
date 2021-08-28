1b30896a301cb1265d112ab3976ce01da521dd2d
Pavel <pav.timashkov@gmail.com>
2021-08-28 20:40:27 +0300

    commit Homework 2

--- script.js
+++ script.js
@@ -10,24 +10,46 @@ console.log(obj.name);
 //const name=prompt("Как тебя зовут?");
 //const name=prompt("Как вас зовут");
 //document.write(`Привет , ${name}`);
-/*const numberOfFilms=+prompt('Сколько фильмов вы посмотрели?');
+const numberOfFilms=+prompt('Сколько фильмов вы посмотрели?');
 const personalMovieDB={
     count:numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
-    privat: false
+    privat: false}
+for (let i=0;i<2;i++){
+    const a=prompt('Один из последних просмотренных фильмов?', ''),
+          b=prompt('На сколько оцените его?', '');
+    
+    if (a!=null && b!=null && a!='' && b!='' && a.length<50){
+        
+              personalMovieDB.movies[a]=b;
+              console.log("Done!")
+            }
+        else {
+            i--; 
+            console.log("Error")   
+        }
+        
+  
+}
 
-};
-const a=prompt('Один из последних просмотренных фильмов?', ''),
-      b=prompt('На сколько оцените его?', ''),
-      с=prompt('Один из последних просмотренных фильмов?', ''), 
-      d=prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+if (personalMovieDB.count<10){
+    document.write("Просмотренно довольно мало фильмов");
+}
+else if (personalMovieDB.count>10 && personalMovieDB.count<30){
+    document.write("Вы любитель!");
+}
+else{ 
+document.write("Вы киноман!");
 
-console.log(personalMovieDB);*/
-for (let i=0;i<10;i++){
+}
+
+
+
+
+console.log(personalMovieDB);
+/*for (let i=0;i<10;i++){
     if (i===6){
         //break;
         continue;
@@ -35,3 +57,4 @@ for (let i=0;i<10;i++){
 
         console.log(i);
     }
+*/
